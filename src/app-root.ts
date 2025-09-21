20604d75211f8a5bec9276999476546d79f0c1dd
fred <fred.jp@bellotalabs.com>
2025-09-21 23:07:04 +0800

    feat: 优化路由

--- src/app-root.ts
+++ src/app-root.ts
@@ -2,7 +2,8 @@ import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { Router } from '@vaadin/router';
 
-let isLoggedIn = false; // 模拟登录状态
+// let isLoggedIn = false; // 模拟登录状态
+
 
 @customElement('app-root')
 export class AppRoot extends LitElement {
@@ -27,46 +28,13 @@ export class AppRoot extends LitElement {
           }
         ]
       },
-      
-      {
-        path: '/register',
-        component: 'login-layout',
-        action: async () => {
-          await import('./layouts/login-layout.js');
-        },
-        children: [
-          {
-            path: '/',
-            component: 'register-view',
-            action: async () => {
-              await import('./views/register-view.js');
-            }
-          }
-        ]
-      },
-      {
-        path: '/login',
-        component: 'login-layout',
-        action: async () => {
-          await import('./layouts/login-layout.js');
-        },
-        children: [
-          {
-            path: '/',
-            component: 'login-view',
-            action: async () => {
-              await import('./views/login-view.js');
-            }
-          }
-        ]
-      },
       {
         path: '/about',
         component: 'main-layout',
-        action: async (_, commands) => {
-          if (!isLoggedIn) {
-            return commands.redirect('/login');
-          }
+        action: async (_) => {
+          // if (!isLoggedIn) {
+          //   return commands.redirect('/login');
+          // }
           await import('./layouts/main-layout.js');
         },
         children: [
@@ -86,26 +54,63 @@ export class AppRoot extends LitElement {
           }
         ]
       },
-      {
-        path: '(.*)',
-        component: 'error-layout',
-        action: async () => {
-          await import('./layouts/error-layout.js');
-        },
-        children: [
-          {
-            path: '/',
-            component: 'not-found-view',
-            action: async () => {
-              await import('./views/not-found-view.js');
-            }
-          }
-        ]
-      }
+      registerR,
+      loginR,
+      ErrBy
     ]);
   }
 
   render() {
     return html`<div id="outlet"></div>`;
   }
+}
+
+const ErrBy = {
+  path: '(.*)',
+  component: 'error-layout',
+  action: async () => {
+    await import('./layouts/error-layout.js');
+  },
+  children: [
+    {
+      path: '/',
+      component: 'not-found-view',
+      action: async () => {
+        await import('./views/not-found-view.js');
+      }
+    }
+  ]
+}
+const registerR = {
+  path: '/register',
+  component: 'login-layout',
+  action: async () => {
+    await import('./layouts/login-layout.js');
+  },
+  children: [
+    {
+      path: '/',
+      component: 'register-view',
+      action: async () => {
+        await import('./views/register-view.js');
+      }
+    }
+  ]
+}
+
+const loginR = {
+  path: '/login',
+  component: 'login-layout',
+  action: async () => {
+    await import('./layouts/login-layout.js');
+  },
+  children: [
+    {
+      path: '/',
+      component: 'login-view',
+      action: async () => {
+        await import('./views/login-view.js');
+      }
+    }
+  ]
 }
