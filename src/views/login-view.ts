66f9e8107f9329b0cda3603234d66c8f5e8ac768
fred <fred.jp@bellotalabs.com>
2025-10-13 10:36:55 +0800

    feat: 优化代码，使用crypto-js,代替aes-js

--- src/views/login-view.ts
+++ src/views/login-view.ts
@@ -4,7 +4,7 @@ import { consume } from '@lit/context';
 import { Router } from '@vaadin/router';
 import { authContext } from '../contexts/auth-context';
 import type { AuthContext } from '../contexts/auth-context';
-import aesjs from 'aes-js';
+import CryptoJS from 'crypto-js'
 
 @customElement('login-view')
 export class LoginView extends LitElement {
@@ -104,10 +104,23 @@ export class LoginView extends LitElement {
       console.log(`用户: ${this._username}, 密码: ${this._password}`);
 
 
-      const usernameEnCoded = encryptToHex(this._username);
-      const passwordEnCoded = encryptToHex(this._password);
+const SecretPs = "Secret Passphrase"
+
+      const usernameEnCoded = CryptoJS.AES.encrypt(this._username, SecretPs);
+
+
+      const usernameDecrypted = CryptoJS.AES.decrypt(usernameEnCoded, SecretPs);
+
+
+      const passwordEnCoded = CryptoJS.AES.encrypt(this._username, SecretPs);
+
+
+      const passwordDecrypted = CryptoJS.AES.decrypt(passwordEnCoded, SecretPs);
+
+
 
       console.log(`用户: ${usernameEnCoded}, 密码: ${passwordEnCoded}`);
+      console.log(`用户: ${usernameDecrypted}, 密码: ${passwordDecrypted}`);
 
       // 模拟登录成功
       this._auth?.login();
@@ -120,31 +133,31 @@ export class LoginView extends LitElement {
 }
 
 
-function encryptToHex(text: string): string {
-  // An example 128-bit key
-  const key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+// function encryptToHex(text: string): string {
+//   // An example 128-bit key
+//   const key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-  // Convert text to bytes (text must be a multiple of 16 bytes)
-  const textBytes = aesjs.utils.utf8.toBytes(text);
+//   // Convert text to bytes (text must be a multiple of 16 bytes)
+//   const textBytes = aesjs.utils.utf8.toBytes(text);
 
-  var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-  var encryptedBytes = aesCtr.encrypt(textBytes);
+//   var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+//   var encryptedBytes = aesCtr.encrypt(textBytes);
 
-  // To print or store the binary data, you may convert it to hex
-  const encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-  return encryptedHex
-  // "104fb073f9a131f2cab49184bb864ca2"
+//   // To print or store the binary data, you may convert it to hex
+//   const encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+//   return encryptedHex
+//   // "104fb073f9a131f2cab49184bb864ca2"
 
-  // // When ready to decrypt the hex string, convert it back to bytes
-  // var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+//   // // When ready to decrypt the hex string, convert it back to bytes
+//   // var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
 
-  // // The cipher-block chaining mode of operation maintains internal
-  // // state, so to decrypt a new instance must be instantiated.
-  // var aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
-  // var decryptedBytes = aesCbc.decrypt(encryptedBytes);
+//   // // The cipher-block chaining mode of operation maintains internal
+//   // // state, so to decrypt a new instance must be instantiated.
+//   // var aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
+//   // var decryptedBytes = aesCbc.decrypt(encryptedBytes);
 
-  // // Convert our bytes back into text
-  // var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-  // console.log(decryptedText);
-  // // "TextMustBe16Byte"
-}
+//   // // Convert our bytes back into text
+//   // var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+//   // console.log(decryptedText);
+//   // // "TextMustBe16Byte"
+// }
